fb141febb02e010b32ada9231955e948da6fe9a9
victor diaz <vdiaz@amentum.net>
2022-04-27 17:10:38 -0500

    View Buttons Implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'extraInfo', loadChildren: () => import('./components/extra-info/extra-info.module').then(m => m.ExtraInfoModule) }, 
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) }, 
   { path: '**', redirectTo: 'home'},
 
